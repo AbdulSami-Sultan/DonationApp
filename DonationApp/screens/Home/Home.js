19a887a65929cbd96ad803b99fd1b11ee63d3660
Abdul Sami Sultan <56970013+AbdulSami-Sultan@users.noreply.github.com>
2024-03-16 18:00:58 +0500

    Badge Component Added.

--- DonationApp/screens/Home/Home.js
+++ DonationApp/screens/Home/Home.js
@@ -4,6 +4,7 @@ import Header from '../../components/Header/Header';
 import globalStyle from '../../assets/styles/globalStyle';
 import Button from '../../components/Button/Button';
 import Tab from '../../components/Tab/Tab';
+import Badge from '../../components/Badge/Badge';
 
 const Home = () => {
   return (
@@ -19,6 +20,7 @@ const Home = () => {
 
       <Tab title={'Highlight'} />
       <Tab title={'Highlight'} isInactive={true} />
+      <Badge title="Enviornment" />
     </SafeAreaView>
   );
 };
