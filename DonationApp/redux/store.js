2cd492781c3bd752f80772744da87a6dd230e6f1
Abdul Sami Sultan <56970013+AbdulSami-Sultan@users.noreply.github.com>
2024-03-23 23:38:21 +0500

    Logger Added

--- DonationApp/redux/store.js
+++ DonationApp/redux/store.js
@@ -1,13 +1,16 @@
 import {combineReducers} from '@reduxjs/toolkit';
 import {configureStore} from '@reduxjs/toolkit';
 import User from './reducers/User';
-
+import {logger} from 'redux-logger';
 const rootReducer = combineReducers({
   user: User,
 });
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: getDefaultMiddleware => {
+    return getDefaultMiddleware().concat(logger);
+  },
 });
 
 export default store;
--- DonationApp/screens/Home/Home.js
+++ DonationApp/screens/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {Pressable, SafeAreaView, Text, View} from 'react-native';
 import Header from '../../components/Header/Header';
 import globalStyle from '../../assets/styles/globalStyle';
 import Button from '../../components/Button/Button';
@@ -11,14 +11,18 @@ import Search from '../../components/Search/Search';
 import SingleDonationItem from '../../components/SingleDonationItem/SingleDonationItem';
 import {horizontalScale} from '../../assets/styles/scaling';
 
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {updateFirstName} from '../../redux/reducers/User';
 
 const Home = () => {
   const user = useSelector(state => state.user);
-  console.log(user);
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      <Header title={user.firstName} />
+      <Header title={user.firstName + ' ' + user.lastName} />
+      <Pressable onPress={() => dispatch(updateFirstName({firstName: 'Sami'}))}>
+        <Text>Press me to change first name</Text>
+      </Pressable>
     </SafeAreaView>
   );
 };
