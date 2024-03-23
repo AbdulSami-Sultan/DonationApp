e9865340c259f3ad88d8aa5dd3b215dd21234d56
Abdul Sami Sultan <56970013+AbdulSami-Sultan@users.noreply.github.com>
2024-03-23 23:56:15 +0500

    Persisting Store added

--- DonationApp/redux/store.js
+++ DonationApp/redux/store.js
@@ -1,16 +1,30 @@
-import {combineReducers} from '@reduxjs/toolkit';
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import User from './reducers/User';
-import {logger} from 'redux-logger';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistReducer, persistStore} from 'redux-persist';
+
 const rootReducer = combineReducers({
   user: User,
 });
 
+const configuration = {
+  key: 'root',
+  storage: AsyncStorage,
+  version: 1,
+};
+
+// Creating a new persisted reducer with the configuration and root reducer
+const persistedReducer = persistReducer(configuration, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: getDefaultMiddleware => {
-    return getDefaultMiddleware().concat(logger);
+    return getDefaultMiddleware({
+      serializableCheck: false,
+    });
   },
 });
 
 export default store;
+export const persistor = persistStore(store);
