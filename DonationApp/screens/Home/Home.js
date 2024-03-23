12399dbebd6ece56505a9f36db17f1ac8666eabc
Abdul Sami Sultan <56970013+AbdulSami-Sultan@users.noreply.github.com>
2024-03-23 23:23:21 +0500

    Redux ToolKit added

--- DonationApp/screens/Home/Home.js
+++ DonationApp/screens/Home/Home.js
@@ -11,50 +11,14 @@ import Search from '../../components/Search/Search';
 import SingleDonationItem from '../../components/SingleDonationItem/SingleDonationItem';
 import {horizontalScale} from '../../assets/styles/scaling';
 
+import {useSelector} from 'react-redux';
+
 const Home = () => {
+  const user = useSelector(state => state.user);
+  console.log(user);
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      {/* <Header title={'Azzahri A.'} type={1} />
-      <Button
-        title={'Donate'}
-        onPress={() => {
-          console.log('You just pressed me!');
-        }}
-      />
-      <Button title={'Donation'} isDisabled={true} />
-
-      <Tab title={'Highlight'} />
-      <Tab title={'Highlight'} isInactive={true} />
-      <Badge title="Enviornment" />
-      <FontAwesomeIcon icon={faSearch} /> */}
-      <Search
-        onSearch={value => {
-          console.log(value);
-        }}
-      />
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          paddingHorizontal: horizontalScale(24),
-        }}>
-        <SingleDonationItem
-          uri={
-            'https://img.pixers.pics/pho_wat(s3:700/FO/44/24/64/31/700_FO44246431_ab024cd8251bff09ce9ae6ecd05ec4a8.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/stickers-cactus-cartoon-illustration.jpg.jpg'
-          }
-          badgeTitle={'Enviornment'}
-          donationTitle={'Tree Cactus'}
-          price={44}
-        />
-        <SingleDonationItem
-          uri={
-            'https://img.pixers.pics/pho_wat(s3:700/FO/44/24/64/31/700_FO44246431_ab024cd8251bff09ce9ae6ecd05ec4a8.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/stickers-cactus-cartoon-illustration.jpg.jpg'
-          }
-          badgeTitle={'Enviornment'}
-          donationTitle={'Tree Cactus'}
-          price={44}
-        />
-      </View>
+      <Header title={user.firstName} />
     </SafeAreaView>
   );
 };
