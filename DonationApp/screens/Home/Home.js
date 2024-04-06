bec1190310c59797571237b1f27e353c078963d2
Abdul Sami Sultan <56970013+AbdulSami-Sultan@users.noreply.github.com>
2024-04-06 13:48:46 +0500

    Donation Item Row Flow added

--- DonationApp/screens/Home/Home.js
+++ DonationApp/screens/Home/Home.js
@@ -124,21 +124,22 @@ const Home = () => {
         {donationItems.length > 0 && (
           <View style={style.donationItemsContainer}>
             {donationItems.map(value => (
-              <SingleDonationItem
-                onPress={selectedDonationId => {
-                  console.log(selectedDonationId);
-                }}
-                donationItemId={value.donationItemId}
-                uri={value.image}
-                donationTitle={value.name}
-                badgeTitle={
-                  categories.categories.filter(
-                    val => val.categoryId === categories.selectedCategoryId,
-                  )[0].name
-                }
-                key={value.donationItemId}
-                price={parseFloat(value.price)}
-              />
+              <View style={style.singleDonationItem} key={value.donationItemId}>
+                <SingleDonationItem
+                  onPress={selectedDonationId => {
+                    console.log(selectedDonationId);
+                  }}
+                  donationItemId={value.donationItemId}
+                  uri={value.image}
+                  donationTitle={value.name}
+                  badgeTitle={
+                    categories.categories.filter(
+                      val => val.categoryId === categories.selectedCategoryId,
+                    )[0].name
+                  }
+                  price={parseFloat(value.price)}
+                />
+              </View>
             ))}
           </View>
         )}
