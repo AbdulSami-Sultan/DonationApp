56f428d75ecece6adf951360dae0daac80c75984
Abdul Sami Sultan <56970013+AbdulSami-Sultan@users.noreply.github.com>
2024-08-18 16:40:18 +0500

    Login Screen Added

--- DonationApp/assets/styles/globalStyle.js
+++ DonationApp/assets/styles/globalStyle.js
@@ -1,11 +1,14 @@
 import {StyleSheet} from 'react-native';
-
+import {verticalScale} from './scaling';
 const globalStyle = StyleSheet.create({
   backgroundWhite: {
     backgroundColor: '#FFFFFF',
   },
   flex: {
     flex: 1,
+  },
+  marginBottom24: {
+    marginBottom: verticalScale(24),
   },
 });
 
